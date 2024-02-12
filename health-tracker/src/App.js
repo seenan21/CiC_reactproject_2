fe048341f331448c1905e9fe2a275a96489db6df
Seenan <seenan21@gmail.com>
2024-02-11 22:53:31 -0800

    created progress post, need to try out with sample data then figure out how to update

--- health-tracker/src/App.js
+++ health-tracker/src/App.js
@@ -4,15 +4,19 @@ import { useState } from 'react';
 import ActivityForm from './components/ActivityLog/ActivityForm'
 import ActivityLog from './components/ActivityLog/ActivityLog'
 import ProgressForm from './components/ProgressTracker/ProgressForm'
+import ProgressPost from './components/ProgressTracker/ProgressPost';
 
 function App() {
   const [posts, setPosts] = useState([])
   const [progress, setProgress] = useState([0,0,"1900-01-01"])
+
+  
   return (
     <div className="App">
       {/*    <header className="App-header">
          Fitness Tracker
        </header> */}
+      <ProgressPost goals={progress} />
       <ProgressForm progress = {progress} setProgress={setProgress} />
       <ActivityForm setPosts={setPosts} />
       {posts && <ActivityLog posts={posts} />}
--- health-tracker/src/components/ProgressTracker/ProgressPost.js
+++ health-tracker/src/components/ProgressTracker/ProgressPost.js
@@ -6,13 +6,39 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 const ProgressPost = ({ goals }) => {
+    const dataPoints = [];
     
+    const options = {
+        title: {
+            text: "Weekly goals for week" + String(goals[2]),
+        },
+        axisY: {
+            title: "Calories burned",
+            includeZero: false,
+            suffix: "cal",
+            //set max
+            maximum: goals[1],
+            viewportmaximum: goals[1],
+        },
+        axisX: {
+            title: "Activity throughout the week",  
+            interval: 1,
+            
+        },
+        data: [{		
+                  theme: "light2",		
+                  type: "line",
+                  xValueFormatString: "D’th’ MMMM hh:mm tt	",
+				  yValueFormatString: "#,###,.##cal",
+				  dataPoints: dataPoints
+         }]
+    }
 
 
     return (
         <div>
             <h2>Progress Tracker</h2>
-            <CanvasJSChart   />
+            <CanvasJSChart  options={options}/>
         </div>
     )
 
